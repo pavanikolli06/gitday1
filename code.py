a8fa0e0769236ff7c1976d027b152401fe03a764
pavanikolli06 <150009880+pavanikolli06@users.noreply.github.com>
2024-12-30 18:18:36 +0530

    Update code.py

---
+++ code.py
@@ -0,0 +1,18 @@
+# THE LEAP YEAR CODE
+year=int(input())
+leap=False
+if year%100==0 and year%400!=0:
+    leap=False
+elif year%4==0:
+    leap=True
+else:
+    leap=False
+print(leap)
+
+
+# CONVERTING THE TEMPERATURE IN CELUSIUS TO FAHRENHEIT AND KELVIN
+c=int(input("enter temperature in celsius:"))
+F=c*(9/5)+32
+K=273+c
+print(f"temperature in Fahrenheit:{F}")
+print(f"temperature in Kelvin:{K}")
